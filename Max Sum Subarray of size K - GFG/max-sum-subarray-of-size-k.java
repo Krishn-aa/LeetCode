9acb4c655be46b0348a53b0ce70dbceaa887c260
Krishna Anand <83511292+Krishn-aa@users.noreply.github.com>
2023-10-06 17:49:26 +0530

    Added solution - LeetHub

---
+++ Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int k, ArrayList<Integer> arr,int n){
+        int i=0;
+        int j=0;
+        long max_sum=0;
+        long sum=0;
+        while(j<n){
+            sum+=arr.get(j);
+            if(j-i+1<k)
+                j++;
+                
+            else{
+                max_sum=Math.max(max_sum,sum);
+                sum-=arr.get(i);
+                i++;
+                j++;
+            }
+        }
+        return max_sum;
+    }
+}
